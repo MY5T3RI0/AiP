aef0a33b90f03a5ffb7517fabc73f27fddb3d8e0
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-10-23 22:16:08 +0300

    edited num 2 in LR2

--- Labs/LR2/num2/num2.cpp
+++ Labs/LR2/num2/num2.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+
     double a = 4;
     double b = 7;
     double c = 2;
@@ -19,6 +21,24 @@ int main()
     const double e_2x = pow(e, 2 * x);
     const double e__mx = pow(e, -sqrt(x));
     const double ctgx3 = 1 / tan(pow(x,3));
+
+    if (x < 0)
+    {
+        cout << "Подкоренное выражение не должно быть отрицательно";
+        return 0;
+    }
+
+    if (tan(pow(x, 3)) == 0)
+    {
+        cout << "Знаменатель не должен быть = 0";
+        return 0;
+    }
+
+    if (cos(pow(x, 3)) == 0)
+    {
+        cout << "Знаменатель не должен быть = 0";
+        return 0;
+    }
 
     cout << "y = " << arctg3_x - 0.75*pow(10,-15) + e_2x - e__mx + ctgx3;
 }
